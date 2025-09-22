555138afc84ada5e94cd4e56592ae35ab7633e76
JactusTheCactus <threateninglyamusing@gmail.com>
2025-09-22 03:09:49 -0700

    Update script.py

--- script.py
+++ script.py
@@ -2,13 +2,13 @@ import json,yaml
 with open("data.yaml", "r") as f:
 	data = yaml.safe_load(f)
 del data["config"]
-with open(f"data.json", "w") as f:
-	json.dump(data,f,indent="\t")
 if not getattr(data,"title",None):
 	data["title"] = " ".join([
 		data["theme"],
 		"/".join(data["genres"])
 	])
+with open(f"data.json", "w") as f:
+	json.dump(data,f,indent="\t")
 with open(f"README.md", "w") as f:
 	data = yaml.dump(data,sort_keys=False)
 	data = "\n".join([
