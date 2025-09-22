47f1b7074e636bd8d296a7b55933cfdac071606c
JactusTheCactus <threateninglyamusing@gmail.com>
2025-09-22 14:27:53 -0700

    Update script.py

--- script.py
+++ script.py
@@ -18,7 +18,9 @@ with open(f"README.md", "w") as f:
 		"## Theme",
 		listToBullets(data["theme"]),
 		"## Software",
-		"\n".join(list(map([f"### {k}\n`{v}`" for k,v in data["software"].items()]))),
+		"\n".join(list(map([
+			f"### {k}\n`{v}`" for k,v in data["software"].items()
+		]))),
 		"## Concept",
 		re.sub(
 			r":(?: null)?$",
