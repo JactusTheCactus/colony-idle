f34de2323c9a1958796541d1fc4602905cd0f0a1
JactusTheCactus <threateninglyamusing@gmail.com>
2025-09-22 02:34:27 -0700

    Update script.py

--- script.py
+++ script.py
@@ -5,6 +5,7 @@ del data["config"]
 with open(f"data.json", "w") as f:
 	json.dump(data,f,indent="\t")
 if not getattr(data,"title",None):
+	print(data)
 	data.title = " ".join([
 		data.theme,
 		"/".join(data.genres)
