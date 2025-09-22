039a34272ac517bb5c891207824a757098a48936
JactusTheCactus <threateninglyamusing@gmail.com>
2025-09-22 02:22:20 -0700

    Update script.py

--- script.py
+++ script.py
@@ -4,8 +4,12 @@ with open("data.yaml", "r") as f:
 del data["config"]
 with open(f"data.json", "w") as f:
 	json.dump(data,f,indent="\t")
+data = yaml.dump(data,sort_keys=False)
+data.title = data.title if data.title else "".join([
+	data.theme,
+	data.genres[0]
+])
 with open(f"README.md", "w") as f:
-	data = yaml.dump(data,sort_keys=False)
 	f.write("\n".join([
 		"```yaml",
 		data.strip(),
