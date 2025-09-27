0893ac7f40aac8d4383527e5c1dfc47d0f9a88c3
JactusTheCactus (Laptop) <threateninglyamusing@gmail.com>
2025-09-26 23:39:49 -0700

    -

---
+++ page.js
@@ -0,0 +1,14 @@
+import * as pug from "pug";
+import * as fs from "fs";
+function writeFile(path, content) {
+    fs.writeFile(path, content, (err) => {
+        if (err) {
+            console.error("Error writing file (asynchronous with callback):", err);
+        }
+    });
+}
+function compilePug(inputPug, outputHtml = "", type = "html") {
+    writeFile(`${outputHtml ? outputHtml : inputPug}.${type}`, pug.compileFile(`${inputPug}.pug`)({}));
+}
+compilePug("README", undefined, "md");
+compilePug("index");
