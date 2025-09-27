91ea46d41ea20aedb4c508492b24af002f01cb2d
JactusTheCactus (Laptop) <threateninglyamusing@gmail.com>
2025-09-26 22:56:17 -0700

    -

--- action.sh
+++ action.sh
@@ -4,7 +4,7 @@ npm ci > /dev/null
 git config user.email "github-actions[bot]@users.noreply.github.com"
 git config user.name "GitHub Actions Bot"
 rm -rf *.css *.html *.js *.md
-# make
+make
 git add .
 git commit -m "Commit Make changes" || :
 git push origin main
