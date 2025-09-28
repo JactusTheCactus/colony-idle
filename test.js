40a137abb5c471d1d215d1f5b4c0aab04f148c71
JactusTheCactus (Laptop) <threateninglyamusing@gmail.com>
2025-09-28 04:32:08 -0700

    -

---
+++ test.js
@@ -0,0 +1,151 @@
+function capitalize(str) {
+	return str.length > 1 ?
+			[str.charAt(0).toUpperCase(), str.slice(1).toLowerCase()].join("")
+		:	str.toUpperCase();
+}
+function join(arr, joinerMain = ", ", joinerFinal = ", ") {
+	const last = arr.pop();
+	const joined = [arr.join(joinerMain), last].join(joinerFinal);
+	return joined;
+}
+function and(arr) {
+	return join(arr, ", ", " & ");
+}
+function or(arr) {
+	return join(arr, " / ", " / ");
+}
+const lang = {};
+lang.cascadic = [
+	"A",
+	"B",
+	"C",
+	"D",
+	"D_",
+	"E",
+	"E_",
+	"F",
+	"G",
+	"H",
+	"I",
+	"I_",
+	"J",
+	"K",
+	"L",
+	"M",
+	"N",
+	"N_",
+	"O",
+	"O_",
+	"P",
+	"R",
+	"S",
+	"S_",
+	"T",
+	"T_",
+	"U",
+	"U_",
+	"U__",
+	"V",
+	"W",
+	"Y",
+	"Z",
+	"Z_",
+];
+lang.stratic = [];
+[
+	"B",
+	"C",
+	"D",
+	"D_",
+	"F",
+	"G",
+	"H",
+	"J",
+	"K",
+	"L",
+	"M",
+	"N",
+	"N_",
+	"P",
+	"R",
+	"S",
+	"S_",
+	"T",
+	"T_",
+	"V",
+	"W",
+	"X",
+	"Y",
+	"Z",
+	"Z_",
+].forEach(c => {
+	["", "A", "E", "E_", "I", "I_", "O", "O_", "U", "U_"].forEach(v => {
+		lang.stratic.push(c + v);
+	});
+});
+const uni = [
+	{
+		_: "\u0301",
+		d: "\u00F0",
+		n: "\u014B",
+		t: "\u00FE",
+	},
+	{
+		_: "\u030C",
+	},
+];
+const native = {
+	stratic: "s_ra'tik",
+	cascadic: "kaskadik",
+};
+function toUnicode(str) {
+	return str
+		.replace(/(\w)(_{1,2})/g, (match, letter, variant) => {
+			let out =
+				uni[variant.length - 1][letter.toLowerCase()] ||
+				[letter, uni[variant.length - 1]["_"]].join("");
+			if (/\p{Lu}/u.test(letter)) {
+				return out.toUpperCase();
+			} else {
+				return out.toLowerCase();
+			}
+		})
+		.normalize("NFC");
+}
+Object.entries(lang).forEach(([k, v]) => {
+	console.log(
+		[
+			`# ${capitalize(k)} \`[${capitalize(toUnicode(native[k]))}]\``,
+			and(
+				v.map(i => {
+					const reg = toUnicode(i);
+					return `\`${capitalize(reg)}\``;
+				}),
+			),
+			null,
+		].join("\n"),
+	);
+});
+[
+	[
+		"For both scripts, diacritics create separate letters, not variants",
+		"(since latin alphabet variants just add diacritics onto characters to make new ones, unlike cyrillic variants, which, generally, make entirely new symbols)",
+	],
+	[
+		`${and("T D".split(/\s+/).map(i => `\`${i}_\``))} are separate phonemes, differing in voicing`,
+		`(${and("\u03B8 d_".split(/\s+/).map(i => `\`/${i}/\``))})`,
+	],
+	"`'` is the Emphasis mark, and follows the emphasized syllable, though is absent from `Cascadic`",
+	[
+		"`Stratic` is an Abugida, with each `CV` pair listed",
+		'(`X` is the `"Null Consonant"`, hence why it\'s absent from `Cascadic`, an alphabet)',
+	],
+	`\`Cascadic\` is an Alphabet, with each Letter (${or(["C", "V"].map(i => `\`${i}\``))}) pair listed`,
+	"`Stratic` consonants have no inherent value",
+	"`U__` is absent from `Stratic`, as the dialect does not have the sound, but the `Cascadic` dialect does",
+].forEach((n, i) => {
+	if (n instanceof Array) {
+		n = n.join(" ");
+	}
+	console.log(`${i + 1}. ${toUnicode(n).replace(/^(.*?)\.?$/g, "$1.")}`);
+});
---
+++ test.md
@@ -0,0 +1,15 @@
+# Cascadic `[Kaskadik]`
+
+`A`, `B`, `C`, `D`, `Ð`, `E`, `É`, `F`, `G`, `H`, `I`, `Í`, `J`, `K`, `L`, `M`, `N`, `Ŋ`, `O`, `Ó`, `P`, `R`, `S`, `Ś`, `T`, `Þ`, `U`, `Ú`, `Ǔ`, `V`, `W`, `Y`, `Z` & `Ź`
+
+# Stratic `[Śra'tik]`
+
+`B`, `Ba`, `Be`, `Bé`, `Bi`, `Bí`, `Bo`, `Bó`, `Bu`, `Bú`, `C`, `Ca`, `Ce`, `Cé`, `Ci`, `Cí`, `Co`, `Có`, `Cu`, `Cú`, `D`, `Da`, `De`, `Dé`, `Di`, `Dí`, `Do`, `Dó`, `Du`, `Dú`, `Ð`, `Ða`, `Ðe`, `Ðé`, `Ði`, `Ðí`, `Ðo`, `Ðó`, `Ðu`, `Ðú`, `F`, `Fa`, `Fe`, `Fé`, `Fi`, `Fí`, `Fo`, `Fó`, `Fu`, `Fú`, `G`, `Ga`, `Ge`, `Gé`, `Gi`, `Gí`, `Go`, `Gó`, `Gu`, `Gú`, `H`, `Ha`, `He`, `Hé`, `Hi`, `Hí`, `Ho`, `Hó`, `Hu`, `Hú`, `J`, `Ja`, `Je`, `Jé`, `Ji`, `Jí`, `Jo`, `Jó`, `Ju`, `Jú`, `K`, `Ka`, `Ke`, `Ké`, `Ki`, `Kí`, `Ko`, `Kó`, `Ku`, `Kú`, `L`, `La`, `Le`, `Lé`, `Li`, `Lí`, `Lo`, `Ló`, `Lu`, `Lú`, `M`, `Ma`, `Me`, `Mé`, `Mi`, `Mí`, `Mo`, `Mó`, `Mu`, `Mú`, `N`, `Na`, `Ne`, `Né`, `Ni`, `Ní`, `No`, `Nó`, `Nu`, `Nú`, `Ŋ`, `Ŋa`, `Ŋe`, `Ŋé`, `Ŋi`, `Ŋí`, `Ŋo`, `Ŋó`, `Ŋu`, `Ŋú`, `P`, `Pa`, `Pe`, `Pé`, `Pi`, `Pí`, `Po`, `Pó`, `Pu`, `Pú`, `R`, `Ra`, `Re`, `Ré`, `Ri`, `Rí`, `Ro`, `Ró`, `Ru`, `Rú`, `S`, `Sa`, `Se`, `Sé`, `Si`, `Sí`, `So`, `Só`, `Su`, `Sú`, `Ś`, `Śa`, `Śe`, `Śé`, `Śi`, `Śí`, `Śo`, `Śó`, `Śu`, `Śú`, `T`, `Ta`, `Te`, `Té`, `Ti`, `Tí`, `To`, `Tó`, `Tu`, `Tú`, `Þ`, `Þa`, `Þe`, `Þé`, `Þi`, `Þí`, `Þo`, `Þó`, `Þu`, `Þú`, `V`, `Va`, `Ve`, `Vé`, `Vi`, `Ví`, `Vo`, `Vó`, `Vu`, `Vú`, `W`, `Wa`, `We`, `Wé`, `Wi`, `Wí`, `Wo`, `Wó`, `Wu`, `Wú`, `X`, `Xa`, `Xe`, `Xé`, `Xi`, `Xí`, `Xo`, `Xó`, `Xu`, `Xú`, `Y`, `Ya`, `Ye`, `Yé`, `Yi`, `Yí`, `Yo`, `Yó`, `Yu`, `Yú`, `Z`, `Za`, `Ze`, `Zé`, `Zi`, `Zí`, `Zo`, `Zó`, `Zu`, `Zú`, `Ź`, `Źa`, `Źe`, `Źé`, `Źi`, `Źí`, `Źo`, `Źó`, `Źu` & `Źú`
+
+1. For both scripts, diacritics create separate letters, not variants (since latin alphabet variants just add diacritics onto characters to make new ones, unlike cyrillic variants, which, generally, make entirely new symbols).
+2. `Þ` & `Ð` are separate phonemes, differing in voicing (`/θ/` & `/ð/`).
+3. `'` is the Emphasis mark, and follows the emphasized syllable, though is absent from `Cascadic`.
+4. `Stratic` is an Abugida, with each `CV` pair listed (`X` is the `"Null Consonant"`, hence why it's absent from `Cascadic`, an alphabet).
+5. `Cascadic` is an Alphabet, with each Letter (`C` / `V`) pair listed.
+6. `Stratic` consonants have no inherent value.
+7. `Ǔ` is absent from `Stratic`, as the dialect does not have the sound, but the `Cascadic` dialect does.
