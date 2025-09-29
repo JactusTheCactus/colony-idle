8f95de9d6bf5fffe355c96592a09f42c514df1d4
JactusTheCactus <threateninglyamusing@gmail.com>
2025-09-28 21:50:44 -0700

    Update test.js

--- test.js
+++ test.js
@@ -145,7 +145,7 @@ Object.entries(lang).forEach(([k, v]) => {
 	"`U__` is absent from `Stratic`, as the dialect does not have the sound, but the `Cascadic` dialect does",
 	[
 		"Numerals in both scripts are `base-12`",
-		`(${and(Array.from({length: 12}, (_, i) => i + 1).map(i => `\`${i}\``))})`,
+		`(${and(Array.from({length: 12}, (_, i) => i).map(i => `\`${i}\``))})`,
 	],
 ].forEach((n, i) => {
 	if (n instanceof Array) {
