073c19b9be051f4a5e4738960fd2243e663acabf
JactusTheCactus <threateninglyamusing@gmail.com>
2025-10-03 18:56:10 -0700

    Update mini.sh

--- mini.sh
+++ mini.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 npx html-minifier --collapse-whitespace --remove-comments neo.html -o neo.min.html
-cp neo{.min,}.html neo{.min,}.md
+for f in neo.html neo.min.html; do
+	cp "$f" "${f%.html}.md"
+done
