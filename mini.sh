56e6cc998e40838d68dc610809a88177e0bbc1e9
JactusTheCactus <threateninglyamusing@gmail.com>
2025-10-05 04:34:05 -0700

    Update mini.sh

--- mini.sh
+++ mini.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 for f in neo; do
-	npx html-minifier --collapse-whitespace --remove-comments $f.html -o $f.min.html
+	npx html-minifier --collapse-whitespace --remove-comments "$f.html" -o "$f.min.html"
 	for m in ".min" ""; do
 		cp "$f$m.html" "$f$m.md"
 	done
